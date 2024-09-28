740f7939ad798e952e08724f2c3cc2315d969b96
Alwin Doss <alwindoss84@gmail.com>
2024-09-28 20:10:40 +0530

    add bootstrap
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/8ideaz/manna
 
 go 1.23.1
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/joho/godotenv v1.5.1
+)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,16 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/8ideaz/manna/views"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 // var index *views.View
@@ -11,23 +18,48 @@ import (
 
 func indexHandler(v *views.View) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request for %s", "/")
 		v.Render(w, nil)
 	}
 }
 
 func contactHandler(v *views.View) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request for %s", "/contact")
 		v.Render(w, nil)
 	}
 }
 
 func aboutHandler(v *views.View) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request for %s", "/about")
 		v.Render(w, nil)
 	}
 }
 
 func Run() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+	port := os.Getenv("MANNA_PORT")
+	if port == "" {
+		port = "3000"
+	}
+	addr := fmt.Sprintf(":%s", port)
+
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
 	// index := views.NewView("bootstrap", "web/layouts", "web/pages/index.html")
 	index := views.NewView(&views.ViewOpts{
 		Layout:      "bootstrap",
@@ -49,8 +81,9 @@ func Run() error {
 		Files:       []string{"web/pages/contact.html"},
 	})
 
-	http.HandleFunc("/", indexHandler(index))
-	http.HandleFunc("/about", aboutHandler(about))
-	http.HandleFunc("/contact", contactHandler(contact))
-	return http.ListenAndServe(":3000", nil)
+	r.Get("/", indexHandler(index))
+	r.Get("/about", aboutHandler(about))
+	r.Get("/contact", contactHandler(contact))
+	log.Printf("Listening on %s", addr)
+	return http.ListenAndServe(addr, r)
 }
